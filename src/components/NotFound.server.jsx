d2c74d1a2267ec08570a1fee69a0865dd027676e
Josh Larson <josh.larson@shopify.com>
2021-08-19 10:32:59 -0500

    Add About

--- src/components/NotFound.server.jsx
+++ src/components/NotFound.server.jsx
@@ -1,16 +1,10 @@
-import {useShopQuery, MediaFile} from '@shopify/hydrogen';
 import {Link} from '@shopify/hydrogen/client';
-import gql from 'graphql-tag';
 
-import Layout from './Layout.client';
-import ProductCard from './ProductCard.client';
+import Wrapper from './Wrapper';
 
 export default function NotFound() {
-  const {data} = useShopQuery({query: QUERY});
-  const {products} = data;
-
   return (
-    <Layout>
+    <Wrapper>
       <div className="text-center py-10 md:py-40 max-w-2xl mx-auto space-y-6">
         <h1 className="text-4xl font-extrabold leading-tight">
           We’ve lost this page
@@ -26,79 +20,6 @@ export default function NotFound() {
           Take me to the home page
         </Link>
       </div>
-
-      <hr className="border-gray-300" />
-
-      <h2 className="my-8 text-2xl text-gray-700 font-bold">
-        Products you might like
-      </h2>
-
-      <ul className="grid lg:grid-cols-3 gap-6 my-4">
-        {products.edges.map((edge) => (
-          <li key={edge.node.id}>
-            <ProductCard product={edge.node} />
-          </li>
-        ))}
-      </ul>
-    </Layout>
+    </Wrapper>
   );
 }
-
-const QUERY = gql`
-  fragment NotFoundProductDetails on Product {
-    id
-    title
-    handle
-    priceRange {
-      maxVariantPrice {
-        amount
-        currencyCode
-      }
-      minVariantPrice {
-        amount
-        currencyCode
-      }
-    }
-    variants(first: 1) {
-      edges {
-        node {
-          priceV2 {
-            currencyCode
-            amount
-          }
-          compareAtPriceV2 {
-            currencyCode
-            amount
-          }
-          image {
-            ...ImageFragment
-          }
-          selectedOptions {
-            name
-            value
-          }
-        }
-      }
-    }
-    media(first: 1) {
-      edges {
-        node {
-          ...MediaFileFragment
-        }
-      }
-    }
-  }
-
-  query Products {
-    products(first: 3) {
-      edges {
-        node {
-          id
-          ...NotFoundProductDetails
-        }
-      }
-    }
-  }
-
-  ${MediaFile.Fragment}
-`;
---
+++ src/pages/about.server.jsx
@@ -0,0 +1,18 @@
+import Wrapper from '../components/Wrapper';
+import Heading from '../components/Heading';
+import {html} from '../prose/about.md';
+import Seo from '../components/Seo.client';
+
+export default function About() {
+  return (
+    <Wrapper>
+      <Seo
+        title="About Josh Larson"
+        description="Here's some stuff about Josh Larson. I bet you're glad you found this page."
+      />
+      <Heading className="mb-8">So, a little bit about me...</Heading>
+
+      <div className="prose" dangerouslySetInnerHTML={{__html: html}}></div>
+    </Wrapper>
+  );
+}
