a73f06bec1ad2a6af5cc97b5596bf49f804d161c
Josh Larson <josh.larson@shopify.com>
2021-08-26 14:49:35 -0500

    Add syntax highlighting

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,30 @@ import {defineConfig} from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import hydrogen from '@shopify/hydrogen/plugin';
 import mdPlugin from 'vite-plugin-markdown';
+import hljs from 'highlight.js';
 
 import shopifyConfig from './shopify.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [hydrogen(shopifyConfig), reactRefresh(), mdPlugin({mode: 'html'})],
+  plugins: [
+    hydrogen(shopifyConfig),
+    reactRefresh(),
+    mdPlugin({
+      markdownIt: {
+        highlight: function (str, lang) {
+          if (lang && hljs.getLanguage(lang)) {
+            try {
+              return hljs.highlight(str, {language: lang}).value;
+            } catch (__) {}
+          }
+
+          return ''; // use external default escaping
+        },
+      },
+      mode: 'html',
+    }),
+  ],
   resolve: {
     // This is a shitshow and breaks things. Comment out for now.
     // alias: [{find: /@\/(.+)$/, replacement: path.join(__dirname, './src/$1')}],
