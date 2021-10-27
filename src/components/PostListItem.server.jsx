f52645fa40c300fd8e09df6c951685e130910291
Josh Larson <jplhomer@gmail.com>
2021-10-27 06:52:23 -0500

    Show real external icon

--- src/components/PostListItem.server.jsx
+++ src/components/PostListItem.server.jsx
@@ -1,5 +1,6 @@
 import Link from './Link.client';
 import ViewCounter from './ViewCounter.server';
+import {ExternalLinkIcon} from '@heroicons/react/outline';
 
 export default function PostListItem({post, href, as}) {
   const isExternal = Boolean(post.externalUrl);
@@ -58,7 +59,7 @@ function ExternalMark({url}) {
   return (
     <span className="text-gray-600 dark:text-gray-300 whitespace-no-wrap block md:inline-block">
       <span className="text-xs md:ml-2">{new URL(url).host}</span>
-      {/* <ExternalLinkIcon className="w-4 h-4 inline-block ml-1" /> */}
+      <ExternalLinkIcon className="w-4 h-4 inline-block ml-1" />
     </span>
   );
 }
